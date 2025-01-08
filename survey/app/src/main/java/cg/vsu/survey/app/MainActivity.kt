8902221506da3a443acc050c3d0e1265396e35bb
heathlisss <heathlisss@gmail.com>
2025-01-08 22:28:23 +0300

    Add toolbarSearch

--- survey/app/src/main/java/cg/vsu/survey/app/MainActivity.kt
+++ survey/app/src/main/java/cg/vsu/survey/app/MainActivity.kt
@@ -8,35 +8,39 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import cg.vsu.survey.R
 import cg.vsu.survey.view.home.FeedSurveyFragment
+import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var searchToolbar: MaterialToolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val bottomNavBar = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
 
+        val bottomNavBar = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        searchToolbar = findViewById(R.id.searchToolbar)
 
         bottomNavBar.setOnItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.nav_home -> {
-
+                    setToolbarVisibility(false)
                     loadFragment(FeedSurveyFragment())
                     true
                 }
                 R.id.nav_search -> {
-
-                    loadFragment(FeedSurveyFragment())
+                    setToolbarVisibility(true)
+                    //loadFragment(FeedSurveyFragment())
                     true
                 }
                 R.id.nav_add_survey -> {
-
+                    setToolbarVisibility(false)
                     //loadFragment()
                     true
                 }
                 R.id.nav_profile -> {
-
+                    setToolbarVisibility(false)
                     //loadFragment(ProfileFragment())
                     true
                 }
@@ -52,6 +56,10 @@ class MainActivity : AppCompatActivity() {
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragmentContainer, fragment)
             .commit()
+    }
+
+    private fun setToolbarVisibility(isVisible: Boolean) {
+        searchToolbar.visibility = if (isVisible) View.VISIBLE else View.GONE
     }
 }
 
